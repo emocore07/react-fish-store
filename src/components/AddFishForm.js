5150a0891fdc8b83a69cfa71f6a0b33635ce6499
Alexey Vasilyev <bestspajic@gmail.com>
2017-02-04 22:49:14 +0300

    Add state and fishes

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class AddFishForm extends React.Component {
+  createFish = (event) => {
+    event.preventDefault();
+    const newFish = {
+      name: this.fishName.value,
+      price: this.fishPrice.value,
+      available: this.fishAvailability.value,
+      description: this.fishDescription.value,
+      image: this.fishImage.value
+    };
+    console.log(newFish);
+    this.props.addFish(newFish);
+    this.fishForm.reset();
+  }
+
+  render() {
+    return (
+      <form
+        ref={(input) => {this.fishForm = input}}
+        onSubmit={this.createFish} className="fish-edit">
+        <input
+          type="text"
+          ref={(input) => { this.fishName = input }}
+          placeholder="Fish Name"
+        />
+        <input
+          type="text"
+          ref={(input) => { this.fishPrice = input }}
+          placeholder="Fish Price"
+        />
+        <select ref={(input) => { this.fishAvailability = input }}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold out!</option>
+        </select>
+        <textarea
+          placeholder="Fish Desc"
+          ref={ (input) => { this.fishDescription = input } }
+        >
+        </textarea>
+        <input
+          type="text"
+          ref={ (input) => { this.fishImage = input } }
+          placeholder="Fish Image" />
+        <button type="submit">+ Add Item</button>
+      </form>
+    );
+  }
+}
+
+export default AddFishForm;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,22 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    const initial_state = {
+      fishes: {},
+      order: {}
+    };
+    this.state = initial_state;
+  }
+
+  addFish = (fish) => {
+    const fishes = {...this.state.fishes};
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +27,7 @@ class App extends React.Component {
           <Header tagline="fresh seafood market"/>
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish}/>
       </div>
     );
   }
