7cc252f246f0fdfe8a57b95c24a582b17ffb9288
Alexey Vasilyev <bestspajic@gmail.com>
2017-02-04 22:54:33 +0300

    Sync state to Firebase and Localstorage!

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBC5JQUUawIYpGd1Vs8FKufJJKSaek4yW8",
+  authDomain: "react-catch-of-the-day-spajic.firebaseapp.com",
+  databaseURL: "https://react-catch-of-the-day-spajic.firebaseio.com",
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
   constructor() {
@@ -13,6 +14,31 @@ class App extends React.Component {
       order: {}
     };
     this.state = initial_state;
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+    if(localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`,
+      JSON.stringify(nextState.order)
+    );
   }
 
   addFish = (fish) => {
