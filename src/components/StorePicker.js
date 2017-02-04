4d67413ad07cbacb7729c0008aa2e2fbc70a8f01
Alexey Vasilyev <bestspajic@gmail.com>
2017-02-04 22:43:16 +0300

    Improve StorePicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,7 +3,14 @@ import React from 'react'; // react should be listed in package.json
 // es6 class
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    return (
+      <form className="store-selector">
+        { /* Comments are weird in jsx */ }
+        <h2>Please Enter a Store</h2>
+        <input type="text" required placeholder="Store Name" />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
   }
 }
 
