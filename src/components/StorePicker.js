56bf52eee7835658a3ad34c4e45e98883ed12758
Alexey Vasilyev <bestspajic@gmail.com>
2017-02-04 22:48:05 +0300

    Handle StorePicker submit

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,23 @@ import React from 'react'; // react should be listed in package.json
 import { getFunName } from '../helpers';
 // es6 class
 class StorePicker extends React.Component {
+  goToStore = (event) => {
+    event.preventDefault(); // prevent form submit
+    console.log('You changed URL!');
+    console.log(this.storeInput.value);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         { /* Comments are weird in jsx */ }
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input
+          ref={(input) => { this.storeInput = input } /* reference to access input DOM element*/}
+          type="text"
+          required placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
