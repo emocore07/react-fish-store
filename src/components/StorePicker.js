922f3f5667fbdf98cc89fd34aa4848ef7243fde5
Alexey Vasilyev <bestspajic@gmail.com>
2017-02-04 22:42:03 +0300

    Add first component: StorePicker

---
+++ src/components/StorePicker.js
@@ -0,0 +1,10 @@
+import React from 'react'; // react should be listed in package.json
+
+// es6 class
+class StorePicker extends React.Component {
+  render() {
+    return <p>Hello</p>
+  }
+}
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // let's go!
+// es6 modules, loads to bundle.js in development
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+// #main is element in index.html, 'mounting point'
+render(<StorePicker/>, document.querySelector('#main'));
