ca04b9a2a2cd118fa907bdc87aaae07e0aca7042
Alexey Vasilyev <bestspajic@gmail.com>
2017-02-04 22:46:30 +0300

    Add routing with ReactRouter4

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@
 // es6 modules, loads to bundle.js in development
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css'; // Webpack will import it and provide hot-reload
 import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  );
+}
 
 // #main is element in index.html, 'mounting point'
-render(<App/>, document.querySelector('#main'));
+render(<Root />, document.querySelector('#main'));
